47c60b8ea0cff8ade34f0279b14a357384c64b2a
calebdamian <naranjocaleb@gmail.com>
2022-10-02 20:51:46 -0500

    Added enableCors() to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //necesario para poder llamar a la API desde otro dominio
   await app.listen(3000);
 }
 bootstrap();
