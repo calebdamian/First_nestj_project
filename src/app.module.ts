f9bfa18a794848090a88930899526af5ea29254b
calebdamian <naranjocaleb@gmail.com>
2022-10-04 23:55:07 -0500

    Added some API documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose'; //importar MONGOOSE
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/web-proj'
-    )
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/web-proj')
   ], //se agrega el modulo de MOONGOOSE importado
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+var msg: string = "";
+
+function createDocumentation(msg: string) {
+  msg += "To get all users: GET localhost:{{port}}/user/";
+  msg += "<br/>To get a singleuser: GET localhost:{{port}}/user/:userId";
+  msg += "<br/>To create an user: POST localhost:{{port}}/user/create";
+  msg += "<br/>To update an user: PUT localhost:{{port}}/user/update/:userId";
+  msg += "<br/>To delete an user: DELETE localhost:{{port}}/user/delete/:userId"
+  msg += "<br/><br/><br/> Developed by: Caleb Naranjo"
+  return msg;
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `${createDocumentation(msg)}`;
   }
 }
