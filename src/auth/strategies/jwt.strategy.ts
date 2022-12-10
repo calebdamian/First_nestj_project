e2a80fce2386f953a6689ef7b1538f9db99ab351
calebdamian <naranjocaleb@gmail.com>
2022-12-09 20:02:13 -0500

    Safe commit

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
   async validate(payload: any) {
     console.log(payload);
-    return { username: payload.username };
+    return { email: payload.email };
   }
 }
