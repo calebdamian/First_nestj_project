93a093c0f82dee846876efb6767f115e5a487ca9
calebdamian <naranjocaleb@gmail.com>
2023-01-07 18:04:37 -0500

    fix back

--- dist/users/dto/create-profile.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateUserProfileDto {
-    full_name: string;
-    email: string;
-}
--- dist/users/dto/update-profile.dto.d.ts
+++
@@ -1,4 +0,0 @@
-declare const UpdateUserProfileDto_base: import("@nestjs/mapped-types").MappedType<Partial<unknown>>;
-export declare class UpdateUserProfileDto extends UpdateUserProfileDto_base {
-}
-export {};
